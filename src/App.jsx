f03809e649341f60e38213c19ffb5fffa5f6c1b0
Ezequiel Bravo <126541104+jezbravo@users.noreply.github.com>
2024-11-24 00:12:17 -0300

    Update files

--- src/App.jsx
+++ src/App.jsx
@@ -112,6 +112,7 @@ function Home() {
               className="relative flex w-full items-center justify-center rounded-lg bg-blue-400 p-2 text-white hover:bg-blue-600"
               type="submit"
               disabled={loading}
+              autoFocus
             >
               {loading ? (
                 <>
